148a9d68196b3eeaecfda70aa9c28426beab6a20
Karen Ribeiro <114605114+professorakarenribeiro@users.noreply.github.com>
2023-04-18 18:45:13 -0400

    Add files via upload
    Aula Template Method

---
+++ GoFDesignPatterns/TemplateMethodCenarioBebidas/Cha.java
@@ -0,0 +1,8 @@
+public class Cha extends Bebida {
+  public void preparar() {
+		System.out.println("Infusionando chá em água fervente...");
+	}
+	public void adicionarComplementos() {
+		System.out.println("Adicionando limão...");
+	}
+}
---
+++ GoFDesignPatterns/TemplateMethodTemplate/Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    ConcreteClass c01 = new ConcreteClass();
+    c01.metodoTemplate();
+  }
+}
