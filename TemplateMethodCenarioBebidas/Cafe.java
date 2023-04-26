1bf531e2f93955f276063ffdc7121ee8187261bc
Karen Ribeiro <114605114+professorakarenribeiro@users.noreply.github.com>
2023-04-26 17:06:06 -0400

    Delete TemplateMethodCenarioBebidas directory

--- TemplateMethodCenarioBebidas/Cafe.java
+++
@@ -1,8 +0,0 @@
-public class Cafe extends Bebida {
-  public void preparar() {
-		System.out.println("Coando café com água fervente...");
-	}
-	public void adicionarComplementos() {
-		System.out.println("Adicionando leite e açúcar...");
-	}
-}
--- TemplateMethodCenarioBebidas/Cha.java
+++
@@ -1,8 +0,0 @@
-public class Cha extends Bebida {
-  public void preparar() {
-		System.out.println("Infusionando chá em água fervente...");
-	}
-	public void adicionarComplementos() {
-		System.out.println("Adicionando limão...");
-	}
-}
